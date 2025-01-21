97d43e118cbff738526c799638e62d2727df7a6f
Louis Delleur <louis.delleur@hotmail.com>
2025-01-21 16:51:52 +0100

    1er version du home page navbar et footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavbarComponent} from './features/navbar/navbar.component';
+import {HomeComponent} from './features/home/home.component';
+import {FooterComponent} from './features/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent, HomeComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
